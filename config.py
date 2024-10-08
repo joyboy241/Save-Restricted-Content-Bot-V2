ce6f9bb51662f6eacd25ad2b9d3c06ab6f1c3881
joyboy241 <deepakwalker449@gmail.com>
2024-10-08 09:23:39 +0530

    config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
-# devggn
+# DEEPAK_JANGID
 # Note if you are trying to deploy on vps then directly fill values in ("")
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-DEFAULT_SESSION = getenv("DEFAULT_SESSION", "")
+API_ID = int(getenv("API_ID", "10758818"))
+API_HASH = getenv("API_HASH", "04fbbf6e526799e099494e33c891aa95")
+BOT_TOKEN = getenv("BOT_TOKEN", "7389306950:AAGsgk53l2GhihtPrhyFEYRckbcvKEbGnt0")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1234819262").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://deepakjangidjoyboy:deepak!123@cluster0.vljsovf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "1001680979427")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "1002473297204"))
